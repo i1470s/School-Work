3020c011d32a8643449cf8a5cab6fbc70bc717a2
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-03-31 14:20:38 -0700

    Delete GRADE 9/Python/BraydenViana-Blackjack Project directory

--- GRADE 9/Python/BraydenViana-Blackjack Project/blackjack.py
+++
@@ -1,9 +0,0 @@
-import random
-
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-
-playerTotal = random.choice(cards) + random.choice(cards)
-computerTotal = random.choice(cards) + random.choice(cards)
-
-print('player total is', playerTotal)
-print('computer total is', computerTotal)
